df0662fa68f4811aaf98f20a514638d4fdcc8e61
J-VdS <jeroen.vanderstraeten@ugent.be>
2021-04-10 22:34:29 +0200

    updates

--- src/main/java/fteam/ForceTeamPlugin.java
+++ src/main/java/fteam/ForceTeamPlugin.java
@@ -2,17 +2,23 @@ package fteam;
 
 import arc.*;
 import arc.struct.ObjectMap;
+import arc.struct.Seq;
 import arc.util.*;
 import mindustry.Vars;
+import mindustry.content.UnitTypes;
 import mindustry.game.EventType.*;
 import mindustry.game.Team;
 import mindustry.gen.*;
+import mindustry.mod.Mods;
 import mindustry.mod.Plugin;
-
+import mindustry.world.Tile;
 
 public class ForceTeamPlugin extends Plugin {
-    //private boolean DEBUG = false;
+    private double TEAMPLUGINVERSION = 6.3;
+
     private Team setTeam = null;
+    private String cmdRunner = null; //uuid
+
 
     private ObjectMap<Player, Team> rememberSpectate = new ObjectMap<>();
 
@@ -28,6 +34,12 @@ public class ForceTeamPlugin extends Plugin {
 
         Events.on(PlayerLeave.class, event -> {
             //if team has 0 players --> problem STALL
+            if(event.player.uuid().equals(cmdRunner) && this.setTeam != null){
+                //disable forceteam
+                this.setTeam = null;
+                Call.sendMessage("[orange] > ForceTeam disabled");
+            }
+            /*
             if(setTeam != null) {
                 Team pt = event.player.team();
                 if (!pt.cores().isEmpty() && Groups.player.count(player -> (player.team() == pt && player != event.player)) == 0) {
@@ -44,17 +56,34 @@ public class ForceTeamPlugin extends Plugin {
                 }
                 Call.sendMessage("[orange] forceTeam disabled");
                 setTeam = null;
-                Vars.state.rules.tags.put("forceTeam", "false");
+                Vars.state.rules.tags.put("forceteam", "false");
             }
+
+             */
         });
 
         Events.on(GameOverEvent.class, event -> {
+            /*
             if(setTeam != null){
                 setTeam = null;
-                Call.sendMessage("[orange] forceTeam disabled");
-                Vars.state.rules.tags.put("forceTeam", "false");
+                Call.sendMessage("[orange] forceteam disabled");
+                Vars.state.rules.tags.put("forceteam", "false");
             }
+             */
         });
+
+        Timer.schedule(this::checkCompatibility, 3f);
+    }
+
+    private void checkCompatibility(){
+        Mods.LoadedMod temp = Vars.mods.list().find(m -> m.name.equalsIgnoreCase("teamplugin"));
+        if(temp != null){
+            if(Strings.parseDouble(temp.meta.version, 0) < TEAMPLUGINVERSION){
+                Log.err("<forceTeamPlugin> Teamplugin is outdated! Mininmum required version @ ", TEAMPLUGINVERSION);
+            }
+        }else{
+            Log.info("<forceTeamPlugin> Compatible with TeamPlugin @", TEAMPLUGINVERSION);
+        }
     }
 
     //register commands that run on the server
@@ -65,13 +94,21 @@ public class ForceTeamPlugin extends Plugin {
     //register commands that player can invoke in-game
     @Override
     public void registerClientCommands(CommandHandler handler){
+        handler.<Player>register("test", "kill", (args, player)-> {
+            if (player.unit().type != UnitTypes.alpha && player.unit().type != UnitTypes.beta && player.unit().type != UnitTypes.gamma) {
+                player.clearUnit();
+                player.deathTimer = Player.deathDelay + 1f;
+            }
+        });
+
         handler.<Player>register("forceteam", "[team] [transfer_players]","[scarlet]Admin only[] For info use the command without arguments.", (args, player) -> {
                 if(!player.admin()){
                     player.sendMessage("[scarlet]Admin only");
                     return;
                 }
 
-                if (!Vars.state.rules.pvp) {
+                //check if sandbox or pvp
+                if (!Vars.state.rules.pvp && !Vars.state.rules.infiniteResources) {
                     player.sendMessage("[scarlet]Only possible in PVP mode.\n");
                     return;
                 }
@@ -85,34 +122,36 @@ public class ForceTeamPlugin extends Plugin {
                     Call.infoMessage(player.con, sbinfo.toString());
                     return;
                 }
-                Vars.state.rules.tags.put("forceTeam", "false");
+                //disable first
+                disableFT();
                 // select team
                 switch (args[0]) {
                     case "off":
-                        setTeam = null;
+                        this.setTeam = null;
+                        player.sendMessage("You disabled forceTeam");
                         Call.sendMessage(" [orange] forceTeam disabled");
                         //randomize again?
                         return;
                     case "sharded":
-                        setTeam = Team.sharded;
+                        this.setTeam = Team.sharded;
                         break;
                     case "blue":
-                        setTeam = Team.blue;
+                        this.setTeam = Team.blue;
                         break;
                     case "crux":
-                        setTeam = Team.crux;
+                        this.setTeam = Team.crux;
                         break;
                     case "derelict":
-                        setTeam = Team.derelict;
+                        this.setTeam = Team.derelict;
                         break;
                     case "green":
-                        setTeam = Team.green;
+                        this.setTeam = Team.green;
                         break;
                     case "purple":
-                        setTeam = Team.purple;
+                        this.setTeam = Team.purple;
                         break;
                     default:
-                        setTeam = null;
+                        this.setTeam = null;
                         player.sendMessage("[scarlet]ABORT: Team not found[] - available teams:");
                         for (int i = 0; i < 6; i++) {
                             if (!Team.baseTeams[i].cores().isEmpty()) {
@@ -121,11 +160,16 @@ public class ForceTeamPlugin extends Plugin {
                         }
                         return;
                 }
-                if (setTeam.cores().isEmpty()) {
-                    player.sendMessage("[scarlet]ABORT: The selected team has 0 cores...");
+                //check if the selected team has cores
+                if (this.setTeam.cores().isEmpty()) {
+                    player.sendMessage(Strings.format("[scarlet]ABORT: The selected team (@) has 0 cores...", this.setTeam.name));
                     return;
                 }
-                Vars.state.rules.tags.put("forceTeam", "true");
+                //enable forceTeam
+                Call.sendMessage(Strings.format("[orange]Forceteam is enabled - selected: @", this.setTeam.name));
+                this.cmdRunner = player.uuid();
+                enableFT();
+
 
                 if(args.length == 2){
                     if (!Strings.canParseInt(args[1])) {
@@ -133,14 +177,7 @@ public class ForceTeamPlugin extends Plugin {
                         return;
                     }
                     if(Strings.parseInt(args[1]) == 1){
-                        for(Player p: Groups.player){
-                            if(!p.admin()){
-                                Call.setPlayerTeamEditor(p, setTeam);
-                                p.team(setTeam);
-                                p.snapSync();
-                                p.unit().kill();
-                            }
-                        }
+                        switchPlayers();
                     }
                 }
         });
@@ -155,15 +192,44 @@ public class ForceTeamPlugin extends Plugin {
             }else {
                 player.sendMessage(String.format("[green] > try to move all players (except admins) to %s", setTeam.name));
 
-                for (Player p : Groups.player) {
-                    if (!p.admin() && p.team() != setTeam) {
-                        Call.setPlayerTeamEditor(p, setTeam);
-                        p.team(setTeam);
-                        p.updateUnit();
-                        p.unit().kill();
-                    }
-                }
+                switchPlayers();
             }
         });
+    }
+
+    private void switchPlayers(){
+        Tile coreTile = this.setTeam.core().tileOn();
+        float x = coreTile.drawx();
+        float y = coreTile.drawy();
+
+        Seq<Player> currentPlayers = Groups.player.copy(new Seq<Player>());
+        for(int i=0; i<currentPlayers.size; i++){
+            Player p = currentPlayers.get(i);
+            if(!p.admin() && p!=null){
+                Call.setPlayerTeamEditor(p, setTeam);
+                p.team(setTeam);
+                p.snapSync();
+                if(p.unit().type != UnitTypes.alpha && p.unit().type != UnitTypes.beta && p.unit().type != UnitTypes.gamma){
+                    p.clearUnit();
+                    p.deathTimer = Player.deathDelay + 1f;
+                }else {
+                    Call.setPosition(p.con, x, y);
+                    p.unit().set(x, y);
+                    p.snapSync();
+                }
+            }
+        }
+    }
+
+    private void disableFT(){
+        this.setTeam = null;
+        Vars.state.rules.tags.put("forceteam", "false");
+        Log.info(Vars.state.rules.tags.getBool("forceteam"));
+    }
+
+    private void enableFT(){
+        //this.setTeam = team;
+        Vars.state.rules.tags.put("forceteam", "true");
+        Log.info(Vars.state.rules.tags.getBool("forceteam"));
     }
 }
